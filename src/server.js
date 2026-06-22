fa4a28dc2aa06f2e15344debe7cf33cad359f506
SAMUEL RABELO DA SILVA <samuel.silva300@senaisp.edu.br>
2026-06-22 09:46:42 -0300

    atualizações

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use('/images', express.static(path.resolve('uploads')));
 app.use('/', routes);
 
 initializeDatabase().then(() => {
